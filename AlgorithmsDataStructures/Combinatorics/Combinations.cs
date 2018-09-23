486f447e48497be79271e3e7b46d64da4236ad54
silver2687 <silver2687@gmail.com>
2018-09-23 23:00:00 +0200

    add Combinations implementations in new Combinatorics namespace

---
+++ AlgorithmsDataStructures/Combinatorics/Combinations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace silver2687.Algorithms.Combinatorics
+{
+    public class Combinations
+    {
+        public IEnumerable<T[]> Generate<T>(IList<T> set, int k)
+        {
+            var result = new T[k];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+
+            while (stack.Count > 0)
+            {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+
+                while (value < k)
+                {
+                    result[index++] = set[++value];
+                    stack.Push(value);
+
+                    if (index == k)
+                    {
+                        yield return result;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
